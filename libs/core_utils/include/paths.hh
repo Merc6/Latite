af9a9d7ac82bb21d7850e93dd39425f8736b359b
Orng <aydinyarbrough12@gmail.com>
2025-06-07 22:02:18 -0500

    added nodiscard attribute to path functions

--- libs/core_utils/include/paths.hh
+++ libs/core_utils/include/paths.hh
@@ -2,8 +2,13 @@
 
 namespace latite::core_utils::paths {
 
+[[nodiscard]]
 std::filesystem::path get_root_dir();
+
+[[nodiscard]]
 std::filesystem::path get_roaming_dir();
+
+[[nodiscard]]
 std::filesystem::path get_client_dir();
 
 } // namespace latite::core_utils::paths
