da311437447539411b1ed498e99267ec632e448e
Orng <aydinyarbrough12@gmail.com>
2025-06-07 22:26:14 -0500

    added include guard and modified function signatures

--- libs/core_utils/include/paths.hh
+++ libs/core_utils/include/paths.hh
@@ -1,14 +1,15 @@
+#pragma once
 #include <filesystem>
 
 namespace latite::core_utils::paths {
 
 [[nodiscard]]
-std::filesystem::path get_root_dir();
+std::filesystem::path const& get_root_dir();
 
 [[nodiscard]]
-std::filesystem::path get_roaming_dir();
+std::filesystem::path const& get_roaming_dir();
 
 [[nodiscard]]
-std::filesystem::path get_client_dir();
+std::filesystem::path const& get_client_dir();
 
 } // namespace latite::core_utils::paths
