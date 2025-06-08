8074c93dbc5dca7beacc89f797c7b19c5139ef2e
Orng <aydinyarbrough12@gmail.com>
2025-06-08 10:11:30 -0500

    add log method and delete copy constructor/assignment

--- libs/core_utils/include/log.hh
+++ libs/core_utils/include/log.hh
@@ -13,12 +13,15 @@ enum class Level {
 
 class Logger {
   public:
+    Logger(Logger const&) = delete;
+    Logger& operator=(Logger const&) = delete;
+
     Logger const& get_logger() {
         static auto const GLOBAL_LOGGER = Logger();
         return GLOBAL_LOGGER;
     };
 
-    Logger(Logger const& _logger) = delete;
+    void log(Level level, std::string_view msg);
 
   private:
     Logger() {};
